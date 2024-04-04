1da3de39703c760bef7aa94e1c05cbbc070bbbc6
Bosko Kulusic <kulusicbosko@gmail.com>
2024-04-04 23:25:47 +0200

    init

---
+++ back-end/src/main/java/ftn/isa/SpringBootExampleApplication.java
@@ -0,0 +1,26 @@
+package ftn.isa;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootExampleApplication.class, args);
+
+	}
+
+}
