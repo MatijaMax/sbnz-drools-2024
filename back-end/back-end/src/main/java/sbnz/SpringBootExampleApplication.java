842cccab4d8feaaac9d240303ffd4dcbb4b1e7b1
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2024-04-05 02:44:09 +0200

    back-end init

---
+++ back-end/back-end/src/main/java/sbnz/SpringBootExampleApplication.java
@@ -0,0 +1,31 @@
+package sbnz;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+	@Bean
+	public Validator validator() {
+		ValidatorFactory validatorFactory = Validation.byDefaultProvider().configure().buildValidatorFactory();
+		return validatorFactory.getValidator();
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootExampleApplication.class, args);
+
+	}
+
+}
