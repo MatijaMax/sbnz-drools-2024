b245b3e2b7a24434648627bd193b9ff61ebe0264
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2024-04-05 03:20:30 +0200

    front-end init

---
+++ front-end/sbnz/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { BoilerplateComponent } from './components/boilerplate/boilerplate.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'companies', pathMatch: 'full'},
+  { path: 'boilerplate', component: BoilerplateComponent},
+  { path: 'register', component: RegisterComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    BoilerplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
